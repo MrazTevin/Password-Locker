fba47f7550a41c55f6f82625beda0b8d4f503895
MrazTevin <millatevin@gmail.com>
2018-01-19 12:31:24 +0300

    switched things up

--- account.py
+++ account.py
@@ -1,2 +1,4 @@
  class Account:
      """Will generate new instance of account"""
+
+ def __init__(self,user_name,password)
--- user.py
+++ user.py
@@ -2,7 +2,7 @@ class User:
     """Will generate instances of user"""
     account_list = []
       """this is a class variable called account_list"""
-    def __init__(self,first_name,last_name,email):
+    def __init__(self,website,user_name,password):
        """Passed in 4 arguements and create 3 instance variables below"""
        self.first_name = first_name
        self.last_name  = last_name
