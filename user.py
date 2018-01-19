4cbfd5b0c1012b244370825fceccbebdf69784fd
MrazTevin <millatevin@gmail.com>
2018-01-19 12:10:14 +0300

    indented properly to clear errors

--- user.py
+++ user.py
@@ -1,8 +1,9 @@
 class User:
     """Will generate instances of user"""
-   account_list = [] #empty
-   def __init__(self,first_name,last_name,email):
+    account_list = []
+
+    def __init__(self,first_name,last_name,email):
        """Passed in 4 arguements"""
        self.first_name = first_name
        self.last_name  = last_name
-       self.email = email
+       self.email =email
