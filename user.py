72621c7952820c63c0b9a273648622e61359ddb6
MrazTevin <millatevin@gmail.com>
2018-01-19 12:18:34 +0300

    extended the docstring content

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ class User:
     account_list = []
       """this is a class variable called account_list"""
     def __init__(self,first_name,last_name,email):
-       """Passed in 4 arguements"""
+       """Passed in 4 arguements and create 3 instance variables below"""
        self.first_name = first_name
        self.last_name  = last_name
        self.email =email
