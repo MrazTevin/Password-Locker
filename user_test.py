15792a26bd17a691e8198d8b49e419cdebac6ed5
MrazTevin <millatevin@gmail.com>
2018-01-19 15:44:28 +0300

    added the tear down method

--- user_test.py
+++ user_test.py
@@ -4,27 +4,33 @@ from user import User
 
 class TestUser(unittest.TestCase):
 
-
     def setUp(self):
-        self.new_user = User("website", "user_name", "password")
+        self.new_user = User("elitesingles.com", "Khalid", "hot96%")
+
+    def tearDown(self):
+        """will cleanup after running the text cases"""
+        User.user_list = []
+
     def test_init(self):
         """
         will test if we initialized our variables properly
         """
-        self.assertEqual(self.new_user.website, "website")
-        self.assertEqual(self.new_user.user_name, "user_name")
-        self.assertEqual(self.new_user.password, "password")
+        self.assertEqual(self.new_user.website, "elitesingles.com")
+        self.assertEqual(self.new_user.user_name, "Khalid")
+        self.assertEqual(self.new_user.password, "hot96%")
+
     def test_save_user(self):
         """is the text_save_user saved into user_list?"""
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
+
     def test_save_multiple_user(self):
         """we want to save multiple users to our user_list"""
         self.new_user.save_user()
-        test_user = User("www.dropbox.com", "MrazHashid", "jheineahiko")
+        test_user = User("googledocs.com", "Nadra", "misru")
         test_user.save_user()
         self.assertEqual(len(User.user_list), 2)
 
 
-if  __name__ ==  '__main__':
+if __name__ == '__main__':
     unittest.main()
