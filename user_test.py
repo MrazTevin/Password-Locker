e5734df17e66348965d99e1f5349e7e34966f439
MrazTevin <millatevin@gmail.com>
2018-01-19 16:15:57 +0300

    testCase to find the user by Username complete

--- user_test.py
+++ user_test.py
@@ -40,6 +40,17 @@ class TestUser(unittest.TestCase):
         self.new_user.delete_user()
         self.assertEqual(len(User.user_list), 1)
 
+    def test_find_user_by_username(self):
+        """to find the user by username and display their information"""
+
+        self.new_user.save_user()
+        test_user = User("elitesingles.com", "Khalid", "hot96%")
+        test_user.save_user()
+
+        found_user = User.find_by_username("Khalid")
+
+        self.assertEqual(found_user.password, test_user.password)
+
 
 if __name__ == '__main__':
     unittest.main()
