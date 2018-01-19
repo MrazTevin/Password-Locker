af98287f5e693a6e623b645a84fd0237faa8265e
MrazTevin <millatevin@gmail.com>
2018-01-19 14:23:31 +0300

    indented properly

--- account.py
+++ account.py
@@ -2,16 +2,8 @@ class Account:
     """Will generate instances of user"""
     user_list = []
     """this is a class variable called account_list"""
-    def __init__(self,user_name,password):
+    def __init__(self, user_name, password):
         """Passed in 4 arguements and create 3 instance variables below"""
         self.user_name = user_name
         self.password = password
-
-
-# class Account:
-#      """Will generate new instance of account"""
-#   account_list = []
-#
-#     def __init__(self, user_name, password):
-#         self.user_name = user_name
-#         self.password = password
+        
