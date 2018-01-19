272f0f8946fcabc89c48ada7b45190a30a088c49
MrazTevin <millatevin@gmail.com>
2018-01-19 15:06:33 +0300

    created the init and save tests

---
+++ user_test.py
@@ -0,0 +1,24 @@
+import unittest
+from user import User
+
+
+class TestUser(unittest.TestCase):
+
+
+    def setUp(self):
+        self.new_user = User("website", "user_name", "password")
+    def test_init(self):
+        """
+        will test if we initialized our variables properly
+        """
+        self.assertEqual(self.new_user.website, "website")
+        self.assertEqual(self.new_user.user_name, "user_name")
+        self.assertEqual(self.new_user.password, "password")
+    def test_save_user(self):
+        """is the text_save_user saved into user_list?"""
+        self.new_user
+
+
+
+if  __name__ ==  '__main__':
+    unittest.main()
