c27986d2a830fa5f27600d669e1785c020d17b8f
MrazTevin <millatevin@gmail.com>
2018-01-19 11:49:33 +0300

    created a class variable to house user details

--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 class User:
     """Will generate instances of user"""
-
+   account_list = [] #empty
    def __init__(self,first_name,last_name,email):
        """Passed in 4 arguements"""
        self.first_name = first_name
