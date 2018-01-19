19369a7fd0b4c42b83f4af4307dac506da601e3f
MrazTevin <millatevin@gmail.com>
2018-01-19 12:34:03 +0300

    modified my instance variables

--- account.py
+++ account.py
@@ -1,4 +1,6 @@
  class Account:
      """Will generate new instance of account"""
 
- def __init__(self,user_name,password)
+ def __init__(self,user_name,password):
+     self.user_name = user_name
+     self.password = password
--- user.py
+++ user.py
@@ -4,6 +4,6 @@ class User:
       """this is a class variable called account_list"""
     def __init__(self,website,user_name,password):
        """Passed in 4 arguements and create 3 instance variables below"""
-       self.first_name = first_name
-       self.last_name  = last_name
-       self.email =email
+       self.website= website
+       self.user_name = user_name
+       self.password = password
