0bb84e8b5204bd2658a022eaec1204d55bd40b95
MrazTevin <millatevin@gmail.com>
2018-01-19 11:45:28 +0300

    passed in 4 arguements and created 3 instance variables

--- user.py
+++ user.py
@@ -1,2 +1,8 @@
 class User:
     """Will generate instances of user"""
+
+   def __init__(self,first_name,last_name,email):
+       """Passed in 4 arguements"""
+       self.first_name = first_name
+       self.last_name  = last_name
+       self.email = email
