c040fb25bd048458abb76dce8bc9ded4a754d51a
MrazTevin <millatevin@gmail.com>
2018-01-19 14:05:06 +0300

    did some changes

--- account.py
+++ account.py
@@ -1,6 +1,17 @@
- class Account:
-     """Will generate new instance of account"""
+class Account:
+    """Will generate instances of user"""
+    user_list = []
+    """this is a class variable called account_list"""
+    def __init__(self,user_name,password):
+        """Passed in 4 arguements and create 3 instance variables below"""
+        self.user_name = user_name
+        self.password = password
 
- def __init__(self,user_name,password):
-     self.user_name = user_name
-     self.password = password
+
+# class Account:
+#      """Will generate new instance of account"""
+#   account_list = []
+#
+#     def __init__(self, user_name, password):
+#         self.user_name = user_name
+#         self.password = password
--- user.py
+++ user.py
@@ -1,9 +1,9 @@
 class User:
     """Will generate instances of user"""
-    account_list = []
-      """this is a class variable called account_list"""
+    user_list = []
+    """this is a class variable called account_list"""
     def __init__(self,website,user_name,password):
-       """Passed in 4 arguements and create 3 instance variables below"""
-       self.website= website
-       self.user_name = user_name
-       self.password = password
+        """Passed in 4 arguements and create 3 instance variables below"""
+        self.website= website
+        self.user_name = user_name
+        self.password = password
