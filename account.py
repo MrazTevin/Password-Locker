b70968fe3b9fa68e5eb4d64c1ff32863ba38cf9d
MrazTevin <millatevin@gmail.com>
2018-01-20 09:23:08 +0300

    made some few chages

--- account.py
+++ account.py
@@ -1,9 +1,8 @@
 class Account:
-    """Will generate instances of user"""
-    user_list = []
+    """Will generate instances of account"""
+    account_list = []
     """this is a class variable called account_list"""
     def __init__(self, user_name, password):
         """Passed in 4 arguements and create 3 instance variables below"""
         self.user_name = user_name
         self.password = password
-        
