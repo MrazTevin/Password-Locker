651766ebffa98d7c5f0e60f276a2fd00d9688399
MrazTevin <millatevin@gmail.com>
2018-01-20 17:41:17 +0300

    created two new functions

--- account_run.py
+++ account_run.py
@@ -4,3 +4,8 @@ from account import Account
 def create_account(user_name, password):
     new_account = Account(user_name, password)
     return new_account
+
+
+def delete_account(account):
+    """function that will delete an account"""
+    account.delete_account()
