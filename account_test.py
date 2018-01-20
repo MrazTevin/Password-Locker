b52eec94255fb92bd7b8c133326933d8a120f4bf
MrazTevin <millatevin@gmail.com>
2018-01-20 09:16:44 +0300

    my first account test passed

--- account_test.py
+++ account_test.py
@@ -1,2 +1,16 @@
 import unittest
 from account import Account
+
+
+class TestAccount(unittest.TestCase):
+    def setUp(self):
+        self.new_account = Account("Arnold", "@908street")
+
+    def test_init(self):
+        """test if we instanstiated properly"""
+        self.assertEqual(self.new_account.user_name, "Arnold")
+        self.assertEqual(self.new_account.password, "@908street")
+
+    
+if __name__ == '__main__':
+    unittest.main()
